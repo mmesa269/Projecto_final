f5117b7682b7fed5d255678451e631f6d1352204
Jhoan Rodriguez <jrodriguez831@unab.edu.co>
2023-04-18 17:58:05 -0500

    Ajuste

---
+++ app/src/main/java/com/example/projecto_final/AnadirRecetaActivity.java
@@ -0,0 +1,14 @@
+package com.example.projecto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnadirRecetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir_receta);
+    }
+}
--- app/src/main/java/com/example/projecto_final/MainActivity.java
+++ app/src/main/java/com/example/projecto_final/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.projecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txt_usuario, txt_contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        referenciar();
     }
+
+    private void referenciar() {
+
+        txt_usuario = findViewById(R.id.txt_usuario);
+        txt_contraseña = findViewById(R.id.txt_contraseña);
+    }
+
+/*    public void clickIniciarSesion(View view) {
+        String PASS = "1234";
+        String User = "def";
+
+        String passUser = txt_usuario.getText().toString();
+        String useruser = txt_contraseña.getText().toString();
+
+        if (PASS.equals(passUser) && User.equals(useruser)) {
+            Intent miIntent = new Intent(this, busqueda_act.class);
+            startActivity(busqueda_act.class);
+            finish();
+        } else {
+            Toast.makeText(this, "No estas en el registro", Toast.LENGTH_SHORT).show();
+        }
+    }*/
 }
+
