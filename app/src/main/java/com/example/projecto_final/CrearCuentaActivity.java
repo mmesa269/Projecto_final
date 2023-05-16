ab74da36e81949b020ffe97e3e6e715b30340fb9
Jhoan Rodriguez <jrodriguez831@unab.edu.co>
2023-05-16 16:39:35 -0500

    Ajuste5

--- app/src/main/java/com/example/projecto_final/CrearCuentaActivity.java
+++ app/src/main/java/com/example/projecto_final/CrearCuentaActivity.java
@@ -12,9 +12,15 @@ import java.util.ArrayList;
 
 public class CrearCuentaActivity extends AppCompatActivity {
 
+    private TextView txt_usuario;
     private TextView txt_contrasena;
 
     private TextView txt_confirmar_contrasena;
+
+    private String usuario;
+    private String contrasena;
+    private String confirmar_contrasena;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,11 @@ public class CrearCuentaActivity extends AppCompatActivity {
     }
     public void clickCrearCuenta(View view) {
 
-        if (txt_confirmar_contrasena.getText().toString() == txt_contrasena.getText().toString()) {
+        usuario = txt_usuario.getText().toString();
+        contrasena = txt_contrasena.getText().toString();
+        confirmar_contrasena = txt_confirmar_contrasena.getText().toString();
+
+        if (confirmar_contrasena == contrasena) {
             Toast.makeText(this, "Cuenta creada exitosamente", Toast.LENGTH_SHORT).show();
         } else
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
